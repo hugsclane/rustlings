e9efa4e12384e54acd86bbaddcb0414ff5b59f5c
huganardo <mail.hugocl@gmail.com>
2023-08-01 09:08:16 +1000

    inital commit

---
+++ clippy/target/debug/deps/clippy3-e41d4858fbccb87a.d
@@ -0,0 +1,8 @@
+C:\Users\hugan\rustlings\exercises\clippy\target\debug\deps\clippy3-e41d4858fbccb87a.rmeta: clippy3.rs Cargo.toml
+
+C:\Users\hugan\rustlings\exercises\clippy\target\debug\deps\clippy3-e41d4858fbccb87a.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
