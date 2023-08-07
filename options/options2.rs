48fce006d5b6b3aad36b578cd666430d5ddaded0
huganardo <mail.hugocl@gmail.com>
2023-08-07 13:15:09 +1000

    Options completed
    Introduced to important conceptual ideas for the Option type.

--- options/options2.rs
+++ options/options2.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[cfg(test)]
 mod tests {
@@ -13,29 +12,41 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
+
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
         }
     }
-
+        //this is a better tool than match when failure options are abundent and hard to predict?
     #[test]
     fn layered_option() {
         let range = 10;
         let mut optional_integers: Vec<Option<i8>> = vec![None];
+        //make a mutable i8 option vector
 
         for i in 1..(range + 1) {
             optional_integers.push(Some(i));
         }
-
+        //populate optional_integers with option(i)
         let mut cursor = range;
 
         // TODO: make this a while let statement - remember that vector.pop also
         // adds another layer of Option<T>. You can stack `Option<T>`s into
         // while let and if let.
-        integer = optional_integers.pop() {
-            assert_eq!(integer, cursor);
+
+        while let integer = optional_integers.pop().flatten() {
+            // this feels unsafe having cursor be mutable
+            if cursor >= 1 {
+            println!("{} and {}",integer.unwrap(),cursor);
+            assert_eq!(integer.unwrap(), cursor);
             cursor -= 1;
+            }
+            else{
+                break
+            }
         }
+        //Unwrap() seems to work fine but I cant call flatten() twice because
+        //optional_integers is not an iterator?
 
         assert_eq!(cursor, 0);
     }
--- options/options3.rs
+++ options/options3.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
@@ -14,8 +12,11 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
     y; // Fix without deleting this line.
 }
+//ref borrows the variable instead of moving it to p, as such y continues to store the
+//co-ordinates. Match statemens WILL consume anything provided so ref is required if you want to
+//reuse variables, I.E. when printing values important values to the console.
